aac25e16e5ad2eb9ad6e857e492bd26c4db88c10
JoaoCarvalho2000 <46655010+JoaoCarvalho2000@users.noreply.github.com>
2019-12-05 12:26:53 +0000

    menu

--- projetoPOOdanca/src/main.java
+++ projetoPOOdanca/src/main.java
@@ -1,10 +1,80 @@
-public class main {
+import java.io.File;
+
+    public class main {
+
+
+
+
+  
     public static void main(String[] args) {
 
-
-        System.out.println("HI");
-
-
+        File espetaculos = new File ("espetaculos.txt");
+        File bailarinos = new File ("bailarinos.txt");
+        File coreografos = new File ("coreografos.txt");
+        File tecnicos = new File ("tecnicos.txt");
+        int n=1;
+        //Menu
+         System.out.println("1 - Criar Espetáculo\n" +
+                            "2 – Consultar Espetáculo\n" +
+                            "3 - Consultar apenas dia e local de Espetáculo" +
+                            "4 – Modificar dia de Espetáculo\n" +
+                            "5 – Corrigir o nome de um Espetáculo\n" +
+                            "6 – Listar todos os Espetáculos\n" +
+                            "7 - Consultar lotação do Espetáculo\n" +
+                            "8 - Calcular média de assistência\n" + 
+                            "9 - Calcular média de investimento\n" +
+                            "10 - Calcular Lucro de Espetáculo\n" +
+                            "11 - Criar Bailarino\n" +
+                            "12 – Apagar um Bailarino\n" +
+                            "13 - Criar Coreógrafo\n" +
+                            "14 – Apagar um Coreógrafo\n" +
+                            "15 - Criar Técnico\n" +
+                            "16 – Apagar um Técnico\n" +
+                            "17 – Listar todos os Bailarinos\n" +
+                            "18 – Listar todos os Coreógrafos\n" +
+                            "19 – Listar todos os Técnicos\n" +
+                            "20 – Terminar");
+         switch (n) {
+             case 1:
+                 break;
+             case 2:
+                 break;
+             case 3:
+                 break;
+             case 4:
+                 break;
+             case 5:
+                 break;
+             case 6:
+                 break;
+             case 7:
+                 break;
+             case 8:
+                 break;
+             case 9:
+                 break;
+             case 10:
+                 break;
+             case 11:
+                 break;
+             case 12:
+                 break;
+             case 13:
+                 break;
+             case 14:
+                 break;
+             case 15:
+                 break;
+              case 16:
+                 break;
+             case 17:
+                 break;
+             case 18:
+                 break;
+             case 19:
+                 break;
+            
+         }
     }
-
+    
 }
