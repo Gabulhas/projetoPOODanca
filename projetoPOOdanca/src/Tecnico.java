cbdc431e82607d4520df16ab35ae4d6b524128e9
JoaoCarvalho2000 <46655010+JoaoCarvalho2000@users.noreply.github.com>
2019-12-05 11:34:40 +0000

    nada

---
+++ projetoPOOdanca/src/Tecnico.java
@@ -0,0 +1,77 @@
+public class Tecnico {
+    private String Nome;
+    private int idade;
+    private double salario;
+    private float cc;
+    private String genero;
+    private String especificacao;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public float getCc() {
+        return cc;
+    }
+
+    public void setCc(float cc) {
+        this.cc = cc;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEspecificacao() {
+        return especificacao;
+    }
+
+    public void setEspecificacao(String especificacao) {
+        this.especificacao = especificacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Tecnico{" +
+                "Nome='" + Nome + '\'' +
+                ", idade=" + idade +
+                ", salario=" + salario +
+                ", cc=" + cc +
+                ", genero='" + genero + '\'' +
+                ", especificacao='" + especificacao + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        //para fazer depois
+        return Todfiodfdkfje;
+
+    }
+
+
+}
